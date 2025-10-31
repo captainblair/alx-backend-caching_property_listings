57a5f49ba4f6327bf039f938473291826ffb76b1
Engineer Tony <wangolotony4@gmail.com>
2025-10-31 21:54:04 +0300

    Update view to return JSON response

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
 @cache_page(60 * 15)
 def property_list(request):
     properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    data = [{
+        'id': prop.id,
+        'title': prop.title,
+        'description': prop.description,
+        'price': str(prop.price),
+        'location': prop.location,
+        'created_at': prop.created_at.isoformat()
+    } for prop in properties]
+    return JsonResponse({'data': data})
