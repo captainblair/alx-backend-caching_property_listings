1a6c6253a7fae716756869c06adb293dd9d71b80
Engineer Tony <wangolotony4@gmail.com>
2025-10-31 21:58:20 +0300

    Add Redis cache metrics analysis

--- properties/utils.py
+++ properties/utils.py
@@ -1,4 +1,6 @@
 from django.core.cache import cache
+from django_redis import get_redis_connection
+import logging
 from .models import Property
 
 def get_all_properties():
@@ -7,3 +9,22 @@ def get_all_properties():
         properties = Property.objects.all()
         cache.set('all_properties', properties, 3600)
     return properties
+
+def get_redis_cache_metrics():
+    redis_conn = get_redis_connection("default")
+    info = redis_conn.info()
+    
+    keyspace_hits = info.get('keyspace_hits', 0)
+    keyspace_misses = info.get('keyspace_misses', 0)
+    
+    total_requests = keyspace_hits + keyspace_misses
+    hit_ratio = keyspace_hits / total_requests if total_requests > 0 else 0
+    
+    metrics = {
+        'keyspace_hits': keyspace_hits,
+        'keyspace_misses': keyspace_misses,
+        'hit_ratio': hit_ratio
+    }
+    
+    logging.info(f"Redis Cache Metrics: {metrics}")
+    return metrics
